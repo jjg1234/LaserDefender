14cb6ea5a06ca124ad9f5459423911d2d3a0d412
JJG <jjeromegerstch@gmail.com>
2017-03-15 22:33:33 +0100

    Ship controls OK

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float m_VelocityCoef;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetKeyboardInput();
+	}
+
+	void GetKeyboardInput()
+	{
+		Vector3 actualPosition = gameObject.transform.position;
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			gameObject.transform.position  =  new Vector3(Mathf.Clamp(actualPosition.x - m_VelocityCoef * Time.deltaTime,0,100), actualPosition.y, actualPosition.z);
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			gameObject.transform.position = new Vector3(Mathf.Clamp(actualPosition.x + m_VelocityCoef * Time.deltaTime, 0, 100), actualPosition.y, actualPosition.z);
+		}
+	}
+}
