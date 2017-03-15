1bd176724cd714ab6baf983ba74e455c06efe29d
JJG <jjeromegerstch@gmail.com>
2017-03-15 15:58:46 +0100

    Project set-up
    Import the menu system and create the empty game scene.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This class allows us to handle Levels (load,...)
+/// </summary>
+public class LevelManager : MonoBehaviour {
+	public void LoadLevel(string _levelName)
+	{
+		SceneManager.LoadScene(_levelName);
+	}
+
+	public void LoadNextLevel()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1, LoadSceneMode.Single);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	static MusicManager instance = null;
+
+	private void Awake()
+	{
+		if (instance != null)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
