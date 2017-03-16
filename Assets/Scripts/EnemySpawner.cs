631d993bd9ce86d5bbb43ed18d6ad36dbd4483a1
JJG <jjeromegerstch@gmail.com>
2017-03-16 18:08:08 +0100

    6-109
    Enemies in formation and moving

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+	public GameObject EnemyPrefab;
+	private Vector3 m_posMin,m_posMax;
+	private bool m_IsMovingRight = false;
+	public float m_Speed = 1;
+
+	private void AdaptGizmoToFormation()
+	{
+		float SpriteHalfSize = EnemyPrefab.GetComponent<SpriteRenderer>().sprite.bounds.size.x / 2;
+
+		//Getting VectorMin and Max to draw the Gizmo cube around the formation
+		m_posMin = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+		m_posMax = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+
+		foreach (Transform group in transform)
+		{
+			foreach (Transform child in group)
+			{
+				if (child.position.x < m_posMin.x)
+				{
+					m_posMin.x = child.position.x;
+
+				}
+				if (child.position.y < m_posMin.y)
+				{
+					m_posMin.y = child.position.y;
+				}
+
+				if (child.position.x > m_posMax.x)
+				{
+					m_posMax.x = child.position.x;
+
+				}
+				if (child.position.y > m_posMax.y)
+				{
+					m_posMax.y = child.position.y;
+				}
+			}
+		}
+
+		m_posMin -= new Vector3(SpriteHalfSize, SpriteHalfSize, 0);
+		m_posMax += new Vector3(SpriteHalfSize, SpriteHalfSize, 0);
+
+		
+		//Vector3 FormationCenter = Vector3.Lerp(posMin, posMax, 0.5f);
+		//m_FormationSize = posMax;
+		//Gizmos.DrawCube(FormationCenter, new Vector2(Vector2.Distance(new Vector2(posMin.x,0),new Vector2(posMax.x,0)), Vector2.Distance(new Vector2(0,posMin.y), new Vector2(0,posMax.y))));
+	}
+
+	private static void DrawRectangleGizmo(Vector3 _lowLeft, Vector3 _HighRight)
+	{
+		Gizmos.DrawLine(_lowLeft, new Vector3(_lowLeft.x, _HighRight.y, 0));
+		Gizmos.DrawLine(_lowLeft, new Vector3(_HighRight.x, _lowLeft.y, 0));
+		Gizmos.DrawLine(_HighRight, new Vector3(_lowLeft.x, _HighRight.y, 0));
+		Gizmos.DrawLine(_HighRight, new Vector3(_HighRight.x, _lowLeft.y, 0));
+	}
+
+	private void OnDrawGizmos()
+	{
+		AdaptGizmoToFormation();
+		//DrawRectangleGizmo(m_posMin - new Vector3(SpriteHalfSize, SpriteHalfSize, 0), m_posMax + new Vector3(SpriteHalfSize, SpriteHalfSize, 0));
+		DrawRectangleGizmo(m_posMin, m_posMax);
+
+		//Gizmos.DrawWireCube(m_FormationCenter, m_FormationSize);
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		AdaptGizmoToFormation();
+
+		foreach (Transform group in transform)
+		{
+			foreach (Transform child in group)
+			{
+				GameObject enemy = Instantiate(EnemyPrefab, child.position, new Quaternion()) as GameObject;
+				enemy.transform.parent = child;
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (m_IsMovingRight)
+		{
+			transform.position += new Vector3(m_Speed * Time.deltaTime, 0, 0);
+			if (transform.position.x >= m_posMax.x)
+			{
+				m_IsMovingRight = false;
+			}
+		}
+		else
+		{
+			transform.position -= new Vector3(m_Speed * Time.deltaTime, 0, 0);
+
+			if (transform.position.x <= m_posMin.x)
+			{
+				m_IsMovingRight = true;
+			}
+		}
+		//Vector3.Lerp(new Vector3(-100, -100, 0), new Vector3(100, 100, 0), Time.fixedTime);
+
+
+		
+
+		//transform.position = new Vector3(Mathf.Clamp(transform.position.x, m_posMin.x, m_posMax.x), Mathf.Clamp(transform.position.y, m_posMin.y, m_posMax.y), 0);
+	}
+
+	
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
 	public float m_VelocityCoef;
 
+	private float m_Xmin, m_Xmax, SpriteHalfSize;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		SpriteHalfSize = GetComponent<SpriteRenderer>().sprite.bounds.size.x / 2;
+		m_Xmin = Camera.main.ViewportToWorldPoint(new Vector3(0,0,0)).x;
+		m_Xmax = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		GetKeyboardInput();
 	}
 
@@ -22,11 +29,15 @@ public class PlayerController : MonoBehaviour {
 
 		if (Input.GetKey(KeyCode.LeftArrow))
 		{
-			gameObject.transform.position  =  new Vector3(Mathf.Clamp(actualPosition.x - m_VelocityCoef * Time.deltaTime,0,100), actualPosition.y, actualPosition.z);
+			transform.position += Vector3.left * m_VelocityCoef * Time.deltaTime;
 		}
 		if (Input.GetKey(KeyCode.RightArrow))
 		{
-			gameObject.transform.position = new Vector3(Mathf.Clamp(actualPosition.x + m_VelocityCoef * Time.deltaTime, 0, 100), actualPosition.y, actualPosition.z);
+			transform.position += Vector3.right * m_VelocityCoef * Time.deltaTime;
 		}
+
+		//Restrict position
+		float newX = Mathf.Clamp(transform.position.x, m_Xmin + SpriteHalfSize, m_Xmax - SpriteHalfSize);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
 	}
 }
